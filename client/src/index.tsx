fa9494f13741d8db403be86781ac1226cb404283
chris.dickinson <chris.dickinson@autotrader.co.uk>
2020-06-29 08:33:06 +0100

    make it better

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -12,8 +12,8 @@ import {ApolloProvider as ApolloProviderHooks} from 'react-apollo-hooks'
 const cache = new InMemoryCache()
 const link = new HttpLink({
   // uri:'http://localhost:4000/graphql'
-  //uri:'https://react-merg.herokuapp.com/graphql'
-  uri:'/graphql'
+  uri:'https://react-merg.herokuapp.com/graphql'
+  //uri:'/graphql'
 })
 
 const client = new ApolloClient({
