87ea4e1d46bd0efefe11d05d2946ed9e03712c1d
chris.dickinson <chris.dickinson@autotrader.co.uk>
2020-06-30 07:30:04 +0100

    update

--- server.js
+++ server.js
@@ -1,39 +1,15 @@
 const mongoose = require('mongoose');
 require('dotenv').config({path: 'variables.env'});
 
-const path = require('path');
-
 const express = require('express');
-const app = express();
-const cors = require('cors');
+const { ApolloServer } = require('apollo-server-express');
 
-const {ApolloServer} = require('apollo-server');
 const {typeDefs} = require('./schema');
 const {resolvers} = require('./resolvers');
 
 const Font = require('./models/Font');
 
 const PORT = process.env.PORT || 4000
-
-app.use(express.static('public'));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
-
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    playground: process.env.NODE_ENV !== "production",
-    context:{
-        Font
-    }
-})
-
-// server.applyMiddleware({
-//     path: '/my-frontend', // you should change this to whatever you want
-//     app,
-//   });
 
 mongoose    
     .connect(process.env.MONGO_URI,{
@@ -43,5 +19,72 @@ mongoose
     .then(() => console.log('bd connected'))
     .catch((err) => console.error(err))
 
-server.listen({port: PORT})
-    .then(({url}) => console.log(`server listening on ${url}`))    
+
+const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers,
+    context:{
+       Font
+    }
+
+});
+
+const app = express();
+
+server.applyMiddleware({ app });
+
+app.listen({port: PORT})
+    console.log(({url}) => console.log(`server listening on ${url}`))  
+
+
+
+
+// const mongoose = require('mongoose');
+// require('dotenv').config({path: 'variables.env'});
+
+// const {ApolloServer} = require('apollo-server-express');
+// const {typeDefs} = require('./schema');
+// const {resolvers} = require('./resolvers');
+
+// const Font = require('./models/Font');
+
+// const path = require('path');
+
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+
+// app.use(cors());
+
+// const PORT = process.env.PORT || 4000
+
+// app.use(express.static('public'));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+// });
+
+// const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     playground: process.env.NODE_ENV !== "production",
+//     context:{
+//         Font
+//     }
+// })
+
+// server.applyMiddleware({
+//     path: '/my-frontend',
+//     app,
+//   });
+
+// mongoose    
+//     .connect(process.env.MONGO_URI,{
+//         useNewUrlParser:true,
+//         useUnifiedTopology:true
+//     })
+//     .then(() => console.log('bd connected'))
+//     .catch((err) => console.error(err))
+
+// app.listen({port: PORT})
+//      console.log(({url}) => console.log(`server listening on ${url}`))    
